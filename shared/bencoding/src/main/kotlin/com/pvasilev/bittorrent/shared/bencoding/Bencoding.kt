08246c434667dbc4983c2e97f278d986f2605f57
pvasilev <pavel.v96@yandex.com>
2020-04-30 23:53:47 +0300

    fix bencoding for raw binary data

--- shared/bencoding/src/main/kotlin/com/pvasilev/bittorrent/shared/bencoding/Bencoding.kt
+++ shared/bencoding/src/main/kotlin/com/pvasilev/bittorrent/shared/bencoding/Bencoding.kt
@@ -1,16 +1,29 @@
 package com.pvasilev.bittorrent.shared.bencoding
 
+private const val DICTIONARY: Byte = 100
+private const val LIST: Byte = 108
+private const val INTEGER: Byte = 105
+private const val END: Byte = 101
+private const val ZERO: Byte = 48
+private const val NINE: Byte = 57
+private const val COLON: Byte = 58
+
 fun encodeString(text: String) = "${text.length}:$text"
 
-fun decodeString(text: String) = text.substringAfter(":")
+fun decodeString(bytes: ByteArray) = bytes.sliceArray(bytes.indexOf(COLON) + 1 until bytes.size)
+    .toString(Charsets.UTF_8)
+
+fun decodeString(text: String): String = decodeString(text.toByteArray())
 
 fun encodeInt(number: Int) = "i${number}e"
 
-fun decodeInt(text: String): Int {
+fun decodeInt(bytes: ByteArray): Int {
     val regex = "i([0-9]+)e".toRegex()
-    val matchResult = regex.matchEntire(text) ?: throw IllegalStateException()
+    val matchResult = regex.matchEntire(String(bytes)) ?: throw IllegalStateException()
     return matchResult.groupValues[1].toInt()
 }
+
+fun decodeInt(text: String): Int = decodeInt(text.toByteArray())
 
 fun encodeList(list: List<*>): String = list.joinToString(prefix = "l", separator = "", postfix = "e") { value ->
     when (value) {
@@ -22,27 +35,29 @@ fun encodeList(list: List<*>): String = list.joinToString(prefix = "l", separato
     }
 }
 
-fun decodeList(text: String): List<Any> {
+fun decodeList(bytes: ByteArray): List<Any> {
     val result = mutableListOf<Any>()
     var index = 1
 
-    while (index != text.length - 1) {
-        val token = text.substring(index).nextToken()
+    while (index != bytes.size - 1) {
+        val token = bytes.sliceArray(index until bytes.size).nextToken()
 
-        val value = when (text[index]) {
-            'd' -> decodeMap(token)
-            'l' -> decodeList(token)
-            'i' -> decodeInt(token)
-            in '0'..'9' -> decodeString(token)
+        val value = when (bytes[index]) {
+            DICTIONARY -> decodeMap(token)
+            LIST -> decodeList(token)
+            INTEGER -> decodeInt(token)
+            in ZERO..NINE -> decodeString(token)
             else -> throw IllegalStateException()
         }
 
-        index += token.length
+        index += token.size
         result += value
     }
 
     return result
 }
+
+fun decodeList(text: String): List<Any> = decodeList(text.toByteArray())
 
 fun encodeMap(map: Map<*, *>): String = map.asIterable()
     .joinToString(prefix = "d", separator = "", postfix = "e") { (key, value) ->
@@ -55,23 +70,23 @@ fun encodeMap(map: Map<*, *>): String = map.asIterable()
         }
     }
 
-fun decodeMap(text: String): Map<Any, Any> {
+fun decodeMap(bytes: ByteArray): Map<Any, Any> {
     val result = mutableMapOf<Any, Any>()
     var index = 1
     var key: Any? = null
 
-    while (index != text.length - 1) {
-        val token = text.substring(index).nextToken()
+    while (index != bytes.size - 1) {
+        val token = bytes.sliceArray(index until bytes.size).nextToken()
 
-        val value = when (text[index]) {
-            'd' -> decodeMap(token)
-            'l' -> decodeList(token)
-            'i' -> decodeInt(token)
-            in '0'..'9' -> decodeString(token)
+        val value = when (bytes[index]) {
+            DICTIONARY -> decodeMap(token)
+            LIST -> decodeList(token)
+            INTEGER -> decodeInt(token)
+            in ZERO..NINE -> decodeString(token)
             else -> throw IllegalStateException()
         }
 
-        index += token.length
+        index += token.size
 
         if (key == null) {
             key = value
@@ -84,29 +99,33 @@ fun decodeMap(text: String): Map<Any, Any> {
     return result
 }
 
-private fun String.nextToken(startIndex: Int = 0): String =
+fun decodeMap(text: String): Map<Any, Any> = decodeMap(text.toByteArray())
+
+private fun ByteArray.nextToken(startIndex: Int = 0): ByteArray =
     when (this[startIndex]) {
-        'd', 'l' -> {
+        DICTIONARY, LIST -> {
             var endIndex = startIndex + 1
-            while (this[endIndex] != 'e') {
-                endIndex += nextToken(endIndex).length
+            while (this[endIndex] != END) {
+                endIndex += nextToken(endIndex).size
             }
-            substring(startIndex, endIndex + 1)
+            sliceArray(startIndex until endIndex + 1)
         }
-        'i' -> {
-            substring(startIndex).takeWhileInclusive { it != 'e' }
+        INTEGER -> {
+            sliceArray(startIndex until size).takeWhileInclusive { it != END }.toByteArray()
         }
-        in '0'..'9' -> {
-            val length = substring(startIndex)
-                .takeWhile { it in '0'..'9' }
+        in ZERO..NINE -> {
+            val length = slice(startIndex until size)
+                .takeWhile { it in ZERO..NINE }
+                .map { it.toChar() }
+                .joinToString(separator = "")
                 .toInt()
             val endIndex = startIndex + length.digits() + 1 + length
-            substring(startIndex, endIndex)
+            sliceArray(startIndex until endIndex)
         }
         else -> throw IllegalStateException()
     }
 
-private fun String.takeWhileInclusive(pred: (Char) -> Boolean): String {
+private fun ByteArray.takeWhileInclusive(pred: (Byte) -> Boolean): List<Byte> {
     var shouldContinue = true
     return takeWhile {
         val result = shouldContinue
